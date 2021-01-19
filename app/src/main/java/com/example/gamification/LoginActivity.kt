dc98f77af51e4534068268668b85c32b0cee703b
IGA <aujot99@gmail.com>
2021-01-19 13:14:33 +0300

    initial commit

---
+++ app/src/main/java/com/example/gamification/LoginActivity.kt
@@ -0,0 +1,35 @@
+package com.example.gamification
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            startActivity(
+                Intent(this, GameActivity::class.java)
+            )
+            finish()
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
