fd2ea2df38e5ce446018f2e8bcba041617321e2d
IGA <aujot99@gmail.com>
2021-01-23 00:17:46 +0300

    Save instance of clickerScore

--- app/src/main/java/com/example/gamification/ClickFragment.kt
+++ app/src/main/java/com/example/gamification/ClickFragment.kt
@@ -1,39 +1,49 @@
 package com.example.gamification
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_click.*
 import java.math.BigDecimal
-
 
 
 class ClickFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_click, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
-        var clickCounter = BigDecimal("0")
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+
+        var clickCounter = BigDecimal(sharedPref?.getString("clickerScore", "0"))
+
+        clickerScore.text = "$clickCounter"
 
         clickerButton.setOnClickListener {
             clickCounter += BigDecimal("1")
             clickerScore.text = "$clickCounter"
+
+            with (sharedPref?.edit()){
+                this?.putString("clickerScore", clickCounter.toString())
+                this?.apply()
+            }
         }
 
 
-
-        Log.wtf("ok", "okkk")
     }
+
+
+
 
 
 }
